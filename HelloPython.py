110f18247711e7600bd67c2b241c8839d58ca0c8
Min Zayar Maung <minzayarmaung2002@gmail.com>
2023-11-19 13:30:45 +0630

    test1

---
+++ HelloPython.py
@@ -0,0 +1,11 @@
+print("Hello" + " Zayar")
+print("String connecting is done with '+' sign")
+
+
+#HelloSup
+
+name = input("What's your Name ?\n")
+length = len(name)
+
+print("Your Name is : " + name )
+print(length)
---
+++ Testing2.py
@@ -0,0 +1,9 @@
+a = input ("a:\n")
+b = input ("b:")
+
+switch = a
+a = b
+b = switch
+
+print("a:" + a)
+print("b:" + b)
