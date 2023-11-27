b999be15f6ea09bbdf0c71a55ecd0c5677d00299
Min Zayar Maung <minzayarmaung2002@gmail.com>
2023-11-27 20:39:59 +0630

    day-6

---
+++ Day-6/day6.py
@@ -0,0 +1,4 @@
+#Day 6 is just testing while loop in the Robot Website !
+
+# Link >>
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
