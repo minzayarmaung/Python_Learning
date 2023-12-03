dd15c65d09c4160056653a9834dd4fff063ca927
Min Zayar Maung <minzayarmaung2002@gmail.com>
2023-12-03 12:20:05 +0630

    Day8 Start 2

---
+++ Day-8/Day8_Start.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Hello How are you?")
+    print("This is Day 8 of Learning Python")
+    
+greet()
---
+++ Day-8/start_2.py
@@ -0,0 +1,13 @@
+def greet_with(name , location):
+    print(f"Your Name is {name}")
+    print(f"Your Location is {location}")
+    
+greet_with("Zayar" , "Yangon")
+
+
+#Keyword
+def greet_2(name = "Maung" , location = "Mandalay"):
+    print(f"Name : {name}")
+    print(f"Location : {location}")
+    
+greet_2()
