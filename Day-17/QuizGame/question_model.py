c459a8780d09117ecf2239fe06ee6aa748a5869a
Min Zayar Maung <zayarkatsuki@gmail.com>
2024-01-06 22:14:47 +0630

    day17

--- Day-17/QuizGame/question_model.py
+++ Day-17/QuizGame/question_model.py
@@ -1,4 +1,5 @@
 class Question:
-    def __init__(self, text, answer):
-        self.txt = text
-        self.ans = answer
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
