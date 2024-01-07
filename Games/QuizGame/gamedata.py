a45f735fe305d40affda847f67ae269bb88f5ec9
Min Zayar Maung <zayarkatsuki@gmail.com>
2024-01-07 20:55:59 +0630

    Games

---
+++ Games/QuizGame/gamedata.py
@@ -0,0 +1,42 @@
+question_data = [
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.",
+     "correct_answer": "True", "incorrect_answers": [
+        "False"
+    ]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Music",
+     "question": "Arcade Fire&#039;s &#039;The Suburbs&#039; won the Album of the Year award in the 2011 Grammys.",
+     "correct_answer": "True", "incorrect_answers": [
+        "False"
+    ]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "History",
+     "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Albert Einstein had trouble with mathematics when he was in school.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Animals", "question": "The Platypus is a mammal.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing a chicken with snow.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "History",
+     "question": "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Film",
+     "question": "There aren&#039;t any live-action clones in &quot;Star Wars: Episode III - Revenge of the Sith&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Haggis is traditionally ate on Burns Night.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+]
---
+++ Games/QuizGame/main.py
@@ -0,0 +1,12 @@
+from gamedata import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
