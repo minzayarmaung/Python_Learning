bbc5f2595286c72122a1b8049fd06724f1366555
Min Zayar Maung <zayarkatsuki@gmail.com>
2024-01-09 20:28:37 +0630

    Day-18 different shapes

--- Day-18/TurtleGUI/differentShapes.py
+++ Day-18/TurtleGUI/differentShapes.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle , Screen
 
 timmy = Turtle()
@@ -11,37 +12,48 @@ octagon = 360/8
 nonagon = 360/9
 decagon = 360/10
 
+colors = ["red" , "blue" , "yellow" , "pink" , "brown" , "SeaGreen" , "IndianRed" , "DeepSkyBlue"]
+
 for i in range(3):
+    timmy.color(random.choice(colors))
     timmy.right(triangle)
     timmy.forward(100)
 
 for i in range(4):
+    timmy.color(random.choice(colors))
     timmy.right(square)
     timmy.forward(100)
 
 for i in range(5):
+    timmy.color(random.choice(colors))
     timmy.right(pentagon)
     timmy.forward(100)
 
 for i in range(6):
+    timmy.color(random.choice(colors))
     timmy.right(hexagon)
     timmy.forward(100)
 
 for i in range(7):
+    timmy.color(random.choice(colors))
     timmy.right(heptagon)
     timmy.forward(100)
 
 for i in range(8):
+    timmy.color(random.choice(colors))
     timmy.right(octagon)
     timmy.forward(100)
 
 for i in range(9):
+    timmy.color(random.choice(colors))
     timmy.right(nonagon)
     timmy.forward(100)
 
 for i in range(10):
+    timmy.color(random.choice(colors))
     timmy.right(decagon)
     timmy.forward(100)
+
 
 screen = Screen()
 screen.exitonclick()
