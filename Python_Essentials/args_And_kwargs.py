6361ec2549df2042e3d3f12b3ef96059c4512edf
Min Zayar Maung <zayarkatsuki@gmail.com>
2024-01-16 13:49:36 +0630

    python essentials

---
+++ Python_Essentials/args_And_kwargs.py
@@ -0,0 +1,18 @@
+# *args and **kwargs in Functions
+# Here we are exploring the differences between *args and *kwargs 
+# Credit goes to Medium Article
+
+# *args allow our function to take in any number of positional arguments, which will be stored in the tuple args.
+
+def myFunction(a, b, *args):
+    print(f"{a=} {b=} {args=}")
+    #print(f"{a} {b} {args}")
+
+myFunction(1,2,3,4,5,6,7,8,9,10)
+
+# **kwargs allow our function to take in any number of keyword arguments , which will be stored in the dict kwargs.
+
+def myFunction1(a , b , **kwargs):
+    print(f"{a=} {b=} {kwargs=}")
+
+myFunction1(a=1,b=2,c=3,d=4,e=5)
