3a68cdfa8704fb3d29c20b1f5e6339386c5bc57f
Min Zayar Maung <zayarkatsuki@gmail.com>
2024-01-29 21:54:39 +0630

    day-22

--- Day-22/PingPongGame/Main.py
+++ Day-22/PingPongGame/Main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 # Screen
 screen = Screen()
@@ -13,6 +15,9 @@ screen.tracer(0)
 # Paddle
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+
+# Ball
+ball = Ball()
 
 screen.listen()
 
@@ -29,6 +34,18 @@ screen.onkey(left_paddle.go_down, "s")
 # Updating The Screen
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect Collision with the Wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # It Needs to Bounce
+        ball.bounce_y()
+
+    # Detect Collision with Paddle
+    if (ball.distance(right_paddle) < 50 and ball.xcor() > 320 or
+            ball.distance(left_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
 
 screen.exitonclick()
